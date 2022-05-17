2c51d4c773d92da5f5635ffebfae80b2bae63a1f
Phoney Badger <phoneybadgercode.4ikc7@simplelogin.co>
2022-05-17 11:09:14 +0530

    Add different forms and shinies
    - The shiny sprites and sprites for different forms of
    all the pokemon are now being generated

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from typing import List
 import aiohttp
 from PIL import Image
 
-from image import PokemonImage
 import unicode_converter as converter
+from image import PokemonImage
+
 
 async def main() -> None:
     base_url = "https://raw.githubusercontent.com/msikma/pokesprite/master/"
     data_endpoint = "data/pokemon.json"
+    colors = ["regular", "shiny"]
 
     async with aiohttp.ClientSession() as session:
         async with session.get(base_url + data_endpoint) as response:
@@ -20,12 +22,22 @@ async def main() -> None:
         tasks: List[asyncio.Task] = []
         for entry in pokemon_json.items():
             pokemon_details = entry[1]
-            # name = pokemon_details["name"]["eng"]
-            ascii_name = pokemon_details["slug"]["eng"]
-            sprite_endpoint = f"pokemon-gen8/regular/{ascii_name}.png"
-            tasks.append(
-                asyncio.create_task(get_sprite(session, base_url + sprite_endpoint))
-            )
+            name = pokemon_details["slug"]["eng"]
+            form_data = pokemon_details["gen-8"]["forms"]
+            forms: List[str] = []
+            for form_name, form_info in form_data.items():
+                if "is_alias_of" in form_info:
+                    continue
+                forms.append("regular" if form_name == "$" else form_name)
+            for form in forms:
+                sprite_name = name
+                if form != "regular":
+                    sprite_name += f"-{form}"
+                    for color in colors:
+                        sprite_endpoint = f"pokemon-gen8/{color}/{sprite_name}.png"
+                        tasks.append(
+                            asyncio.create_task(get_sprite(session, base_url + sprite_endpoint))
+                        )
 
         sprites = await asyncio.gather(*tasks)
         for sprite in sprites:
